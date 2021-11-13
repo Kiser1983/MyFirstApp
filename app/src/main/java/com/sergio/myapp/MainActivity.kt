2681fee7dd699af89a6cabc5bfe180681292677b
Sergey Antonov <Antonov-s@mail.ru>
2021-11-13 15:52:45 +0400

    Create new design

--- app/src/main/java/com/sergio/myapp/MainActivity.kt
+++ app/src/main/java/com/sergio/myapp/MainActivity.kt
@@ -2,12 +2,29 @@ package com.sergio.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity", "Hello")
+
+        val showButton: Button = findViewById(R.id.show_button)
+
+        showButton.setOnClickListener {
+            showSckbar(it)
+        }
+    }
+
+    private fun showSckbar(view: View) {
+        Snackbar.make(this, view, "Snackbar show", Snackbar.LENGTH_INDEFINITE)
+            .setAction("Action") {
+                Toast.makeText(this, "Toast!", Toast.LENGTH_LONG).show()
+            }
+            .show()
     }
 }
+
